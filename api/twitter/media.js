7acac5b1b29311a0b7bcbcb9e768d522264ad83a
Cory Cook <clovecooks@gmail.com>
2017-07-09 21:30:03 -0700

    Add max_id parameter for paging

--- api/twitter/media.js
+++ api/twitter/media.js
@@ -4,11 +4,13 @@ const client = require('./client');
 const router = express.Router();
 
 router.get('/:username', (req, res) => {
-  const username = req.params.username;
+  const { username } = req.params;
+  const { page: { max_id } = {} } = req.query;
   client.get('search/tweets', {
     q: `@${username} filter:media -filter:retweets`,
     count: 100,
     result_type: 'recent',
+    max_id,
   }).then((response) => {
     res.format({
       'text/html': () => {
@@ -18,8 +20,8 @@ router.get('/:username', (req, res) => {
         res.json(response);
       },
     });
-  }).catch((err) => {
-    res.status(500).json({ error: err });
+  }).catch((error) => {
+    res.status(500).json({ error });
   });
 });
 
