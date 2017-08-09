7af06cf2dec44929cf7a1eaa4c4dfe6671930b84
Cory Cook <clovecooks@gmail.com>
2017-08-09 02:10:52 -0700

    Add live updating.

--- api/api.js
+++ api/api.js
@@ -1,9 +1,7 @@
-
 const express = require('express');
 
 const api = express.Router();
 
-// GET /gallery
 api.use(require('./gallery'));
 
 module.exports = api;
--- api/gallery.js
+++ api/gallery.js
@@ -1,59 +1,10 @@
 const express = require('express');
 const client = require('./util/twitter-client');
-const bigInt = require('big-integer');
+const { transform } = require('./util/twitter-status-parse');
 const querystring = require('querystring');
 
 const router = express.Router();
 
-function mapStatus(statuses) {
-  return statuses.map((status) => {
-    const {
-      entities: { media: defaultMedia = [], hashtags = [] },
-      extended_entities: { media = defaultMedia } = {},
-    } = status;
-    const videos = media.filter(i => ['video', 'animated_gif'].includes(i.type));
-    let video;
-    if (videos.length) {
-      const [{ id_str: id, type, media_url_https: thumb, video_info: { variants = [] } }] = videos;
-      video = { id, isGIF: type === 'animated_gif', thumb, src: variants.map(i => ({ type: i.content_type, url: i.url })) };
-    }
-    return {
-      created: status.created_at,
-      id: status.id_str,
-      text: status.text,
-      username: status.user.screen_name,
-      tags: hashtags.map(i => i.text),
-      images: media
-        .filter(i => i.type === 'photo')
-        .map(i => ({
-          id: i.id_str,
-          url: i.media_url_https,
-          sizes: Object.keys(i.sizes).map(size => Object.assign({ size }, i.sizes[size])),
-        })),
-      video,
-      favorites: status.favorite_count,
-      retweets: status.retweet_count,
-    };
-  });
-}
-
-function transform(statuses, req) {
-  const data = mapStatus(statuses);
-  const base = `https://${req.get('host')}${req.path}`;
-  const first = statuses[0].id_str;
-  const last = bigInt(statuses[statuses.length - 1].id_str).subtract(bigInt.one).toString();
-  const links = {
-    self: `${base}?page[max_id]=${first}`,
-    next: `${base}?page[max_id]=${last}`,
-    first: base,
-  };
-  return { data, links };
-}
-
-/**
- *
- * @param {string|object} query
- */
 function moreTweets(query) {
   return new Promise((resolve) => {
     let options = query;
